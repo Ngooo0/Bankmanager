b4b5ead32a412b1f0170f76706723a576f8e23e6
ngone gueye <mamengonegueye7@gmail.com>
2025-10-26 20:36:59 +0000

    Fix Swagger routes to use proper controller instead of closure

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route pour la documentation Swagger
-Route::get('/api/documentation', function () {
-    return redirect('/docs');
-});
+// Routes pour la documentation Swagger
+Route::get('/api/documentation', [App\Http\Controllers\Api\SwaggerController::class, 'redirect']);
+Route::get('/docs', [App\Http\Controllers\Api\SwaggerController::class, 'index']);
