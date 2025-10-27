bba383d7f98a442d5299f67b054b021ef342f330
ngone gueye <mamengonegueye7@gmail.com>
2025-10-27 21:38:33 +0000

    Add test routes to diagnose deployment issues

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,24 @@ use Illuminate\Support\Facades\Route;
 use L5Swagger\L5Swagger;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'status' => 'ok',
+        'message' => 'BankManager API is running',
+        'timestamp' => now()->toISOString(),
+        'version' => '1.0.0'
+    ]);
+});
+
+// Route de test simple
+Route::get('/test', function () {
+    return response()->json([
+        'status' => 'success',
+        'message' => 'Test route working',
+        'routes' => [
+            'swagger_ui' => url('/api/documentation'),
+            'swagger_json' => url('/api/docs')
+        ]
+    ]);
 });
 
 // Routes L5-Swagger (assets et API)
