fd38e35a148432c4b6d4a5e5621b3d9a7a4202cb
ngone gueye <mamengonegueye7@gmail.com>
2025-10-27 22:30:40 +0000

    Fix Swagger routes conflict - remove duplicate manual routes

--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,4 @@ Route::get('/', function () {
 // Routes L5-Swagger (assets et API)
 L5Swagger::routes();
 
-// Routes pour la documentation Swagger
-Route::get('/api/documentation', function () {
-    return view('vendor.l5-swagger.index');
-})->middleware(['web']);
-//route pour avoir l'interface swagger UI
-Route::get('/docs', function () {
-    return view('vendor.l5-swagger.index');
-})->middleware(['web']);
-// Route pour /api/docs
-Route::get('/api/docs', function () {
-    return view('vendor.l5-swagger.index');
-})->middleware(['web']);
-
 
